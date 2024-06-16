ddb35be0d6943330ed9542a3528bd8ee5f90cdee
gayathriaunni <131193975+gayathriaunni@users.noreply.github.com>
2024-06-16 20:25:00 +0530

    Update to_do_list.py

--- to_do_list.py
+++ to_do_list.py
@@ -1,3 +1,7 @@
+'''A To-Do List application is a useful project that helps users manage and organize their tasks efficiently. This project aims to create a 
+command-line or GUI-based application using Python, allowing users to create, update, and track their to-do lists'''
+
+
 import tkinter as tk
 from tkinter import messagebox
 
