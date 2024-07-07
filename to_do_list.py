7b536d44a5bf87b97f246bbe889e5647f583076a
gayathriaunni <131193975+gayathriaunni@users.noreply.github.com>
2024-07-07 20:52:49 +0530

    Update to_do_list.py

--- to_do_list.py
+++ to_do_list.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
 
-# Function to add a task
 def add_task():
     task = entry_task.get()
     if task:
@@ -10,8 +9,7 @@ def add_task():
         entry_task.delete(0, tk.END)  # Clear the entry textbox
     else:
         messagebox.showwarning("Warning", "You must enter a task.")
-
-# Function to mark a task as complete
+        
 def complete_task():
     if task_listbox.curselection():
         selected_task_index = task_listbox.curselection()[0]
@@ -19,8 +17,7 @@ def complete_task():
         update_task_listbox()
     else:
         messagebox.showwarning("Warning", "You must select a task.")
-
-# Function to delete a task
+        
 def delete_task():
     if task_listbox.curselection():
         selected_task_index = task_listbox.curselection()[0]
@@ -29,7 +26,6 @@ def delete_task():
     else:
         messagebox.showwarning("Warning", "You must select a task.")
 
-# Function to update the listbox with tasks
 def update_task_listbox():
     task_listbox.delete(0, tk.END)
     index = 1
@@ -42,15 +38,13 @@ def update_task_listbox():
         else:
             task_listbox.itemconfig(tk.END, {'fg': 'black'})
         index += 1
-
-# Initialize main application window
+        
 root = tk.Tk()
 root.title("Simple To-Do List")
 
 tasks = []
 button_width = 20 
 
-# Create and place widgets
 task_listbox = tk.Listbox(root, width=50, height=10)
 task_listbox.pack(pady=10)
 
@@ -66,5 +60,4 @@ button_complete_task.pack(pady=5)
 button_delete_task = tk.Button(root, text="Delete Task", bg="lightcoral", width=button_width, command=delete_task)
 button_delete_task.pack(pady=5)
 
-# Start the GUI event loop
 root.mainloop()
